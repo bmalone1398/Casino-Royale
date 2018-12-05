ff611737b487311d06ffe3078e76ea7006af8281
harshav16 <vallurip@purdue.edu>
2018-12-05 15:57:30 -0500

    backend

---
+++ files/__init__.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Index, func
+from flask import Flask
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+
+
+Bootstrap(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://casinoroyale:passpass@casinoroyale.csu6kcem8ioz.us-east-2.rds.amazonaws.com/casinoroyale'
+
+db = SQLAlchemy(app)
+
+app.secret_key = "casinoroyale"
+
+from files import routes
+
+class users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+   
+
+
---
+++ files/routes.py
@@ -0,0 +1,2 @@
+# from files.__init__ import
+from flask_login import login_user, current_user, logout_user, login_required
