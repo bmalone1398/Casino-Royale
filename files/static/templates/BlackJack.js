d7608e5a6f184db7c532f1bf3a2fed46b16455f3
harshav16 <vallurip@purdue.edu>
2018-12-05 18:03:14 -0500

    Push good stuff

---
+++ files/static/templates/BlackJack.js
@@ -0,0 +1,177 @@
+
+/* global Deck */
+
+var prefix = Deck.prefix
+
+var transform = prefix('transform')
+
+var translate = Deck.translate
+
+document.getElementById("hitme").addEventListener("click", hitme);
+document.getElementById("deal").addEventListener("click", deal);
+document.getElementById("stand").addEventListener("click", stand);
+var $container = document.getElementById('container')
+var $phand= document.getElementById('playerHand')
+var $lphand= document.getElementById('leftPlayerHand')
+var $rphand= document.getElementById('rightPlayerHand')
+var $tphand= document.getElementById('topPlayerHand')
+$('hitme').attr('disabled','disabled');
+var deck = Deck()
+
+deck.cards.forEach(function (card, i) {
+  
+  function onTouch () {
+    var card
+    card = Deck.Card(52 + j)
+    card.mount(deck.$el)
+    card.$el.style[transform] = 'scale(0)'
+    card.setSide('front')
+    card.enableDragging()
+    card.enableFlipping()
+   
+  }
+})
+
+function startWinning () {
+  var $winningDeck = document.createElement('div')
+  $winningDeck.classList.add('deck')
+
+  $winningDeck.style[transform] = translate(Math.random() * window.innerWidth - window.innerWidth / 2 + 'px', Math.random() * window.innerHeight - window.innerHeight / 2 + 'px')
+
+  $container.appendChild($winningDeck)
+
+  var side = Math.floor(Math.random() * 2) ? 'front' : 'back'
+
+  for (var i = 0; i < 55; i++) {
+    addWinningCard($winningDeck, i, side)
+  }
+
+  setTimeout(startWinning, Math.round(Math.random() * 1000))
+}
+
+function addWinningCard ($deck, i, side) {
+  var card = Deck.Card(54 - i)
+  var delay = (55 - i) * 20
+  var animationFrames = Deck.animationFrames
+  var ease = Deck.ease
+
+  card.enableFlipping()
+
+  if (side === 'front') {
+    card.setSide('front')
+  } else {
+    card.setSide('back')
+  }
+
+  card.mount($deck)
+  card.$el.style.display = 'none'
+
+  var xStart = 0
+  var yStart = 0
+  var xDiff = -500
+  var yDiff = 500
+
+  animationFrames(delay, 1000)
+    .start(function () {
+      card.x = 0
+      card.y = 0
+      card.$el.style.display = ''
+    })
+    .progress(function (t) {
+      var tx = t
+      var ty = ease.cubicIn(t)
+      card.x = xStart + xDiff * tx
+      card.y = yStart + yDiff * ty
+      card.$el.style[transform] = translate(card.x + 'px', card.y + 'px')
+    })
+    .end(function () {
+      card.unmount()
+    })
+}
+
+
+
+
+
+deck.intro();
+deck.shuffle();
+
+deck.mount($container);
+var deckone;
+var decktwo ;
+var deckthree;
+var deckfour;
+
+var sum=0;
+var altsum=0;
+var i=0;
+
+function deal()
+{
+  deckone= deck.cards.splice(0,2);
+  deckone[0].mount($phand);
+  deckone[0].enableDragging();
+  deckone[0].enableFlipping();
+  deckone[1].mount($phand);
+  deckone[1].enableDragging();
+  deckone[1].enableFlipping();
+  var of=deckone[0].rank
+  if(of>10)
+  {
+    of=10;
+  }
+  sum+=of;
+  if(of==1)
+  {
+    of=11;
+  }
+  altsum+=of;
+
+  of=deckone[1].rank
+  if(of>10)
+  {
+    of=10;
+  }
+  sum+=of;
+  if(of==1)
+  {
+    of=11;
+  }
+  altsum+=of;
+  console.log(altsum)
+  console.log(sum);
+  $('deal').attr('disabled','disabled');
+  $('hitme').removeAttr('disabled');
+}
+function hitme()
+{
+  var len=deckone.length;
+  deckone[len]= deck.cards.splice(0,1);
+  deckone[len][0].mount($phand);
+  deckone[len][0].enableDragging();
+  deckone[len][0].enableFlipping();
+  var oof=deckone[len][0].rank;
+  if(oof>10)
+  {
+    oof=10;
+  }
+  sum+=oof
+  if(oof==1)
+  {
+    oof=11;
+  }
+  altsum+=oof;
+
+  console.log(altsum)
+  if(sum>21)
+  {
+    alert("You are a Loser.")
+  }
+  console.log(sum);
+  
+}
+function stand()
+{
+  $('deal').attr('disabled','disabled');
+  $('hitme').attr('disabled','disabled');
+}
