42815f16d8d1c48d11293e7a2f5834ae67e57d26
bmalone1398 <bmalone1398@gmail.com>
2018-12-06 21:23:17 -0500

    removed extra socket code

--- files/templates/index.js
+++ files/templates/index.js
@@ -1,34 +1,6 @@
 
 var cp = require('child_process')
 var chokidar = require('chokidar')
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/Main.html');
-});
-
-app.get('/dist/deck.js',function(req,res){
-  console.log("Hello")
-  res.sendFile(__dirname + '/dist/deck.js')
-});
-
-app.get('/BlackJack.js',function(req,res){
-  console.log(__dirname)
-  res.sendFile(__dirname + '/BlackJack.js')
-});
-
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
 
 cp.exec('mkdir -p dist')
 
